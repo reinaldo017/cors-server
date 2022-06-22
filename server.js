b9d8116c6a088f95c332f28d1c5d04157c227ea8
Reinaldo J <reinaldojacome12@gmail.com>
2022-06-21 21:11:29 -0500

    whitelist modified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://ravenous-reinaldo017.vercel.app/'],
+    originWhitelist: ['http://localhost:3000', 'https://ravenous-reinaldo017.vercel.app'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
